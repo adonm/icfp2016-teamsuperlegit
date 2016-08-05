9497ba38430b85ae9896f33c39b2ef9d253c6b12
Scott Percival <code@moral.net.au>
2016-08-05 20:56:05 +0800

    make skeletons look nicer

--- src/rendersvg.rs
+++ src/rendersvg.rs
@@ -9,26 +9,26 @@ pub fn draw_svg<N: Num>(shape: Shape<N>, skel: Skeleton<N>, filename: &str) {
 	/* Draw shapes as areas and skeletons as lines */
 	let mut document = Document::new().set("viewBox", (0, 0, 1, 1));
     let mut defs = element::Definitions::new();
-    let mut markerPathStart = element::Path::new()
+    let marker_path_start = element::Path::new()
             .set("fill", "crimson")
             .set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
-            .set("transform", "scale(0.4) translate(10,0)");
-    let mut markerPathEnd = element::Path::new()
+            .set("transform", "scale(0.2) rotate(180) translate(10,0)");
+    let marker_path_end = element::Path::new()
             .set("fill", "crimson")
             .set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
-            .set("transform", "scale(0.4) rotate(180) translate(10,0)");
-    let mut markerStart = element::Marker::new()
+            .set("transform", "scale(0.2) rotate(180) translate(10,0)");
+    let marker_start = element::Marker::new()
             .set("style", "overflow:visible")
             .set("orient", "auto")
             .set("id", "ArrowStart")
-            .add(markerPathStart);
-    let mut markerEnd = element::Marker::new()
+            .add(marker_path_start);
+    let marker_end = element::Marker::new()
             .set("style", "overflow:visible")
             .set("orient", "auto")
             .set("id", "ArrowEnd")
-            .add(markerPathEnd);
-    defs = defs.add(markerStart);
-    defs = defs.add(markerEnd);
+            .add(marker_path_end);
+    defs = defs.add(marker_start);
+    defs = defs.add(marker_end);
     document = document.add(defs);
 
 	for polygon in shape {
@@ -53,8 +53,8 @@ pub fn draw_svg<N: Num>(shape: Shape<N>, skel: Skeleton<N>, filename: &str) {
         let line = element::Line::new()
                 .set("fill", "none")
                 .set("stroke", "crimson")
-                .set("stroke-width", 0.02)
-                .set("stroke-dasharray", "0.02,0.02")
+                .set("stroke-width", 0.015)
+                .set("stroke-dasharray", "0.01,0.01")
                 .set("marker-start", "url(#ArrowStart)")
                 .set("marker-end", "url(#ArrowEnd)")
                 .set("x1", bone.p1.x.to_f64())
