f142298703885bae5b495c6cfc9f8d892c7eb739
Adon Metcalfe <adonm@fastmail.fm>
2016-08-05 22:05:12 +0800

    rendersvg less printing and tabs not spaces

--- src/rendersvg.rs
+++ src/rendersvg.rs
@@ -8,28 +8,28 @@ use core::*;
 pub fn draw_svg<N: Num>(shape: Shape<N>, skel: Skeleton<N>, filename: &str) {
 	/* Draw shapes as areas and skeletons as lines */
 	let mut document = Document::new().set("viewBox", (0, 0, 1, 1));
-    let mut defs = element::Definitions::new();
-    let marker_path_start = element::Path::new()
-            .set("fill", "crimson")
-            .set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
-            .set("transform", "scale(0.2) rotate(180) translate(10,0)");
-    let marker_path_end = element::Path::new()
-            .set("fill", "crimson")
-            .set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
-            .set("transform", "scale(0.2) rotate(180) translate(10,0)");
-    let marker_start = element::Marker::new()
-            .set("style", "overflow:visible")
-            .set("orient", "auto")
-            .set("id", "ArrowStart")
-            .add(marker_path_start);
-    let marker_end = element::Marker::new()
-            .set("style", "overflow:visible")
-            .set("orient", "auto")
-            .set("id", "ArrowEnd")
-            .add(marker_path_end);
-    defs = defs.add(marker_start);
-    defs = defs.add(marker_end);
-    document = document.add(defs);
+	let mut defs = element::Definitions::new();
+	let marker_path_start = element::Path::new()
+		.set("fill", "crimson")
+		.set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
+		.set("transform", "scale(0.2) rotate(180) translate(10,0)");
+	let marker_path_end = element::Path::new()
+		.set("fill", "crimson")
+		.set("d",  "M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z")
+		.set("transform", "scale(0.2) rotate(180) translate(10,0)");
+	let marker_start = element::Marker::new()
+		.set("style", "overflow:visible")
+		.set("orient", "auto")
+		.set("id", "ArrowStart")
+		.add(marker_path_start);
+	let marker_end = element::Marker::new()
+		.set("style", "overflow:visible")
+		.set("orient", "auto")
+		.set("id", "ArrowEnd")
+		.add(marker_path_end);
+	defs = defs.add(marker_start);
+	defs = defs.add(marker_end);
+	document = document.add(defs);
 
 	for polygon in shape {
 		let mut iter = polygon.points.iter();		
@@ -41,7 +41,7 @@ pub fn draw_svg<N: Num>(shape: Shape<N>, skel: Skeleton<N>, filename: &str) {
 			println!("{:?}, {:?}", point.x.to_f64(), point.y.to_f64());
 			data = data.line_to((point.x.to_f64(), point.y.to_f64()));
 		}
-        data = data.close();
+	data = data.close();
 		let path = element::Path::new()
 				.set("fill", "none")
 				.set("stroke", "black")
@@ -49,20 +49,20 @@ pub fn draw_svg<N: Num>(shape: Shape<N>, skel: Skeleton<N>, filename: &str) {
 				.set("d", data);
 		document = document.add(path);
 	}
-    for bone in skel {
-        let skel_data = element::path::Data::new()
-                .move_to((bone.p1.x.to_f64(), bone.p1.y.to_f64()))
-                .line_to((bone.p2.x.to_f64(), bone.p2.y.to_f64()));
-        let skel_path = element::Path::new()
-                .set("fill", "none")
-                .set("stroke", "crimson")
-                .set("stroke-width", 0.015)
-                .set("stroke-dasharray", "0.01,0.01")
-                .set("marker-start", "url(#ArrowStart)")
-                .set("marker-end", "url(#ArrowEnd)")
-                .set("d", skel_data);
-        document = document.add(skel_path);
-    }
+	for bone in skel {
+	let skel_data = element::path::Data::new()
+		.move_to((bone.p1.x.to_f64(), bone.p1.y.to_f64()))
+		.line_to((bone.p2.x.to_f64(), bone.p2.y.to_f64()));
+	let skel_path = element::Path::new()
+		.set("fill", "none")
+		.set("stroke", "crimson")
+		.set("stroke-width", 0.015)
+		.set("stroke-dasharray", "0.01,0.01")
+		.set("marker-start", "url(#ArrowStart)")
+		.set("marker-end", "url(#ArrowEnd)")
+		.set("d", skel_data);
+	document = document.add(skel_path);
+	}
 	// only save when float coords done ok
 	svg::save(format!("{}/{}", BASEPATH, filename), &document).unwrap();
 }
