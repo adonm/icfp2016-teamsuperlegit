cde5dda782fc05e13ffadacdde6c7dc84dd265ae
sqweek <sqweek@gmail.com>
2016-08-05 17:24:46 +0800

    polygon: determine whether poly represents a hole
    As defined by the spec, a polygon whose points are ordered in a clockwise
    fashion represent a hole.

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,6 @@
 use std::f64::INFINITY;
 use std::ops::{Add,Sub,Mul,Div};
+use std::clone::Clone;
 use std::cmp::Ord;
 use std::fmt::Debug;
 use std::str::FromStr;
@@ -9,6 +10,7 @@ use num::bigint::{BigInt,Sign};
 use num::rational::BigRational;
 use num::pow::pow;
 use num::{ToPrimitive,Zero};
+use num::Float;
 
 pub trait ToF64 {
 	fn to_f64(&self) -> f64;
@@ -37,8 +39,8 @@ impl ToF64 for BigRational {
 	}
 }
 
-pub trait Num: Add + Sub + Mul + Div + Sized + FromStr + Debug + Ord + ToF64 {}
-impl<N> Num for N where N: Add + Sub + Mul + Div + Sized + FromStr + Debug + Ord + ToF64 {}
+pub trait Num: Add<Output=Self> + Sub<Output=Self> + Mul + Div + Sized + FromStr + Debug + Ord + ToF64 + Clone {}
+impl<N> Num for N where N: Add<Output=N> + Sub<Output=N> + Mul + Div + Sized + FromStr + Debug + Ord + ToF64 + Clone {}
 
 #[derive(Debug,PartialEq)]
 pub struct Point<T: Num> {
@@ -51,6 +53,7 @@ pub struct Line<T: Num>(pub Point<T>, pub Point<T>);
 
 #[derive(Debug)]
 pub struct Polygon<T: Num> {
+	is_hole: bool,
 	pub points: Vec<Point<T>>,
 }
 
@@ -59,41 +62,43 @@ pub type Shape<T> = Vec<Polygon<T>>;
 pub type Skeleton<T> = Vec<Line<T>>;
 
 impl<T: Num> Add for Point<T> where T: Add<Output=T> {
-	type Output=Point<T>;
-	fn add(self, other: Point<T>) -> Point<T> {
+	type Output=Self;
+	fn add(self, other: Point<T>) -> Self {
 		Point{x: self.x + other.x, y: self.y + other.y}
 	}
 }
 
 impl<T: Num> Sub for Point<T> where T: Sub<Output=T> {
-	type Output=Point<T>;
-	fn sub(self, other: Point<T>) -> Point<T> {
+	type Output=Self;
+	fn sub(self, other: Point<T>) -> Self {
 		Point::<T>{x: self.x - other.x, y: self.y - other.y}
 	}
 }
 
-impl<T: Num> Polygon<T> {
+impl<T: Num> Polygon<T> where T: Sub<Output=T>+Add<Output=T> {
 	pub fn new(points: Vec<Point<T>>) -> Polygon<T> {
-		Polygon{points: points}
+		let clockwise = is_clockwise(&points);
+		Polygon{points: points, is_hole: clockwise}
 	}
 
 	pub fn is_hole(self) -> bool {
-		is_clockwise(self)
+		self.is_hole
 	}
 }
 
-/*fn angle<T: Num>(p0: &Point<T>, p1: &Point<T>) -> f64 where T: Sub<Output=T> {
-	// `p1 - p0` doesn't work for some reason???
-	let dx = p1.x - p0.x;
-	let dy = p1.y - p0.y;
-	return dx.to_f64() / dy.to_f64();
-}*/
+pub fn angle<T: Num>(p0: &Point<T>, p1: &Point<T>) -> f64 where T: Sub<Output=T> {
+	let dx = p1.x.clone() - p0.x.clone();
+	let dy = p1.y.clone() - p0.y.clone();
+	return dx.to_f64().atan2(dy.to_f64());
+}
 
-fn is_clockwise<T: Num>(poly: Polygon<T>) -> bool {
-	for pair in poly.points.windows(2) {
-		//angle(&pair[0], &pair[1]);
+fn is_clockwise<T: Num>(points: &Vec<Point<T>>) -> bool where T: Sub<Output=T>+Add<Output=T> {
+	let n = points.len();
+	let mut sum = (points[0].x.clone() - points[n-1].x.clone()).to_f64() * (points[0].y.clone() + points[n-1].y.clone()).to_f64();
+	for edge in points.windows(2) {
+		sum += (edge[1].x.clone() - edge[0].x.clone()).to_f64() * (edge[1].y.clone() + edge[0].y.clone()).to_f64();
 	}
-	false
+	sum >= 0.0
 }
 
 #[cfg(test)]
@@ -101,10 +106,28 @@ mod tests {
 	use super::*;
 	extern crate num;
 	use self::num::rational::BigRational;
+	use num::Float;
+
+	fn p(x: i32, y: i32) -> Point<i32> {
+		Point{x: x, y: y}
+	}
 
 	#[test]
-	fn test_add() {
-		assert_eq!(Point::<i32>{x: 5, y: 7}, Point{x: 2, y: 4} + Point{x: 3, y: 3});
+	fn test_ops() {
+		assert_eq!(p(5, 7), p(2, 4) + p(3, 3));
+		assert_eq!(p(-1, 1), p(2, 4) - p(3, 3));
+	}
+
+	#[test]
+	fn test_angle() {
+		assert_eq!(45.0.to_radians(), angle(&p(0, 0), &p(1, 1)));
+		assert_eq!(-135.0.to_radians(), angle(&p(1, 1), &p(0, 0)));
+	}
+
+	#[test]
+	fn test_clockwise() {
+		assert!(!Polygon::new(vec!(p(0, 0), p(1, 0), p(1, 1), p(0, 1))).is_hole());
+		assert!(Polygon::new(vec!(p(0, 0), p(0, 1), p(1, 1), p(1, 0))).is_hole());
 	}
 
 	#[test]
