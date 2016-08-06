28a1c1ed18431a25ca3eadeee566836064b28d9c
sqweek <sqweek@gmail.com>
2016-08-07 02:05:37 +0800

    matrix: add transform constructors

--- src/matrix.rs
+++ src/matrix.rs
@@ -11,6 +11,39 @@ fn idx(index: (usize, usize)) -> usize {
 }
 
 impl<N: Num> Matrix33<N> {
+	pub fn scale(sx: N, sy: N) -> Matrix33<N> {
+		Matrix33::new(
+			(sx, N::zero(), N::zero()),
+			(N::zero(), sy, N::zero()),
+			(N::zero(), N::zero(), N::one())
+		)
+	}
+
+	pub fn shear(hx: N, hy: N) -> Matrix33<N> {
+		Matrix33::new(
+			(N::one(), hx, N::zero()),
+			(hy, N::one(), N::zero()),
+			(N::zero(), N::zero(), N::one()),
+		)
+	}
+
+	pub fn rotate(angle: f64 /* in radians */) -> Matrix33<N> {
+		let (s, c) = (angle.sin(), angle.cos());
+		Matrix33::new(
+			(N::from_f64(c), N::from_f64(s), N::zero()),
+			(N::from_f64(-s), N::from_f64(c), N::zero()),
+			(N::zero(), N::zero(), N::one()),
+		)
+	}
+
+	pub fn translate(tx: N, ty: N) -> Matrix33<N> {
+		Matrix33::new(
+			(N::zero(), N::zero(), N::zero()),
+			(N::zero(), N::zero(), N::zero()),
+			(tx, ty, N::one()),
+		)
+	}
+
 	pub fn new(row0: (N, N, N), row1: (N, N, N), row2: (N, N, N)) -> Matrix33<N> {
 		Matrix33{points: [
 			row0.0, row0.1, row0.2,
@@ -19,6 +52,14 @@ impl<N: Num> Matrix33<N> {
 		]}
 	}
 
+	fn refs(&self) -> (&N, &N, &N, &N, &N, &N, &N, &N, &N) {
+		(&self.points[0], &self.points[1], &self.points[2], &self.points[3], &self.points[4], &self.points[5], &self.points[6], &self.points[7], &self.points[8])
+	}
+
+	pub fn det(&self) -> N {
+		let (a, b, c, d, e, f, g, h, i) = self.refs();
+		N::zero() // TODO
+	}
 }
 
 // (row, col)
