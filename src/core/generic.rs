3dfa8d6a9073b485fd3c31c6ad3d49cf86a5c523
Matt James <matthew.stephen.james@gmail.com>
2016-08-07 20:38:17 +0800

    brute force rationals

--- src/core/generic.rs
+++ src/core/generic.rs
@@ -12,9 +12,11 @@ use std::str::FromStr;
 extern crate num;
 use num::rational::BigRational;
 use num::ToPrimitive;
+use self::num::bigint::BigInt;
 
 pub trait SuperLegit {
 	fn to_f64(&self) -> f64;
+    fn to_rat(&self) -> BigRational;
 	fn from_f64(f64) -> Self;
 	fn zero() -> Self;
 	fn one() -> Self;
@@ -23,6 +25,7 @@ pub trait SuperLegit {
 
 impl SuperLegit for i32 {
 	fn to_f64(&self) -> f64 { *self as f64 }
+    fn to_rat(&self) -> BigRational { BigRational::from_f64(self.clone() as f64) }
 	fn from_f64(f: f64) -> Self { f as i32 }
 	fn zero() -> Self { 0 }
 	fn one() -> Self { 1 }
@@ -31,6 +34,7 @@ impl SuperLegit for i32 {
 
 impl SuperLegit for f64 {
 	fn to_f64(&self) -> f64 { *self }
+    fn to_rat(&self) -> BigRational { BigRational::from_f64(self.clone()) }
 	fn from_f64(f: f64) -> Self { f }
 	fn zero() -> Self { 0.0 }
 	fn one() -> Self { 1.0 }
@@ -46,11 +50,66 @@ pub fn divide<N:Num>( a: N, b: N ) -> Option<N> {
     
 }
 
+
+pub fn eq_eps_custom<N: Num, M: Num>(a: &N, b: &M, eps: f64) -> bool {
+  return (b.clone().to_f64() - a.clone().to_f64()).abs() < eps;
+}
+
+
+
+pub fn find_close_rational_point<N: Num>(p: Point<N>) -> Point<BigRational> {
+    
+    let x = match find_close_rational(p.clone().x) {
+        Some(z) => z,
+        None => p.clone().x.to_rat()
+    };
+    let y = match find_close_rational(p.clone().y) {
+        Some(z) => z,
+        None => p.clone().y.to_rat()
+    };
+    
+	let p = Point{
+		x: x,
+		y: y
+	};
+    
+	p
+}
+
+
+pub fn find_close_rational<N:Num>( x: N ) -> Option<BigRational> {
+    
+    for i in 0..100 {
+        
+        for j in 1..100 {
+            
+            let y = BigRational::new(BigInt::from(i), BigInt::from(j));
+            if eq_eps_custom(&x,&y, 0.0000000000001) {
+                return Some(y);
+            }
+            
+            
+            let z = BigRational::new(BigInt::from(-i), BigInt::from(j));
+            if eq_eps_custom(&x,&z, 0.0000000000001) {
+                return Some(z);
+            }
+            
+        }
+        
+    }
+    
+    None
+    
+}
+
+
 impl SuperLegit for BigRational {
 	fn to_f64(&self) -> f64 {
 		// BUG converts very large negatives to positive infinity
 		self.numer().to_f64().unwrap_or(INFINITY) / self.denom().to_f64().unwrap_or(1.0)
 	}
+    
+    fn to_rat(&self) -> BigRational { self.clone() }
 
 	fn from_f64(f: f64) -> Self { 
 		// handle impossible floats
@@ -163,13 +222,26 @@ impl<N: Num> Display for Line<N> {
 #[cfg(test)]
 mod tests {
 	use super::super::tests::*;
+    use super::*;
 
+    extern crate num;
+    use num::ToPrimitive;
+    use self::num::bigint::BigInt;
 	#[test]
 	fn test_ops() {
 		assert_eq!(p(5, 7), p(2, 4) + p(3, 3));
 		assert_eq!(p(-1, 1), p(2, 4) - p(3, 3));
 	}
 
+    
+	#[test]
+    fn find_close_rat_test(){
+        
+        let n = find_close_rational( 0.3333333333333333333333 ).unwrap();
+        assert_eq!(n,BigRational::new(BigInt::from(1), BigInt::from(3)));
+        
+    }
+    
 	#[test]
 	fn test_commutivity() {
 		let (p1, p2) = (p(1.0, 1.5), p(1.25, 2.5));
